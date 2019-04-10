7e052e1319d89f32caff42d1217a901584b65389
Jacob Downs <Sylvaeseel@live.com>
2019-04-10 19:56:16 -0400

    Lots of changes 1.0
    Website redesign v0.1, Updated Current website, reorganized things

---
+++ Public Server/servermods/@CJTF2_PublicServer/mod.cpp
@@ -0,0 +1,12 @@
+name = "CJTF2 Public Server";
+actionName = "Website";
+action = "www.cjtf2.com";
+description = "Combined Joint Task Force 2 Public Server";
+
+tooltip = "Combined Joint Task Force 2 Public Server";
+tooltipOwned = "Combined Joint Task Force 2 Public Server";
+overview = "Our Public Server Mod";
+author = "Sylvaeseel";
+hidePicture = 0;
+hideName = 1;
+requiresRestart = 0;
---
+++ Unit Server/servermods/@CJTF2_Server/mod.cpp
@@ -0,0 +1,12 @@
+name = "CJTF2 Server";
+actionName = "Website";
+action = "www.cjtf2.com";
+description = "Combined Joint Task Force 2 Server";
+
+tooltip = "Combined Joint Task Force 2 Server";
+tooltipOwned = "Combined Joint Task Force 2 Server";
+overview = "Our Server Mod";
+author = "Sylvaeseel";
+hidePicture = 0;
+hideName = 1;
+requiresRestart = 0;
